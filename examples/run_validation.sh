15c19824d423eb5935ef3e725fbd46e2bc0ced58
Taekyung Heo <taekyung@gatech.edu>
2022-07-05 15:36:20 -0400

    Update examples/run_validation.sh

--- examples/run_validation.sh
+++ examples/run_validation.sh
@@ -1,16 +1,17 @@
 #! /bin/bash -v
 
 # Absolue path to this script
+SCRIPT_DIR=$(dirname "$(realpath $0)")
 
 #ASTRASIM="/home/dkadiyala3/Desktop/astra-sim-roofline"
 #ASTRASIM="/nethome/dkadiyala3/astra-sim-roofline"
 
 # Absolute paths to useful directories
-BINARY="../build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra"
-NETWORK="../inputs/network/analytical/validate.json"
-SYSTEM="../inputs/system/validate.txt"
-WORKLOAD="../inputs/workload/validation-microbenchmark.txt"
-STATS="./validation_astra_allreduce_analytical"
+BINARY="${SCRIPT_DIR:?}"/../build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra
+NETWORK="${SCRIPT_DIR:?}"/../inputs/network/analytical/validate.json
+SYSTEM="${SCRIPT_DIR:?}"/../inputs/system/validate.txt
+WORKLOAD="${SCRIPT_DIR:?}"/../inputs/workload/validation-microbenchmark.txt
+STATS="${SCRIPT_DIR:?}"/validation_astra_allreduce_analytical
 
 rm -rf "${STATS}"
 mkdir "${STATS}"
@@ -23,6 +24,4 @@ mkdir "${STATS}"
 --run-name="Transformer_HybridParallel" \
 --num-passes=1 \
 --total-stat-rows=1 \
---stat-row=0 
-
-#--comm-scale=50 \
+--stat-row=0
